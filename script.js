b0fe2626f64131f13819b0de1a398e9aa9cc5e15
alyson.aurelio@gmail.com <alyson.aurelio@gmail.com>
2021-01-23 23:01:14 -0300

    correção código

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+
 let isJumping = false;
+let isGameOver = false;
 let position = 0;
 
 function handleKeyUp(event) {
@@ -16,9 +18,9 @@ function jump() {
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
-            clearInterval(upInterval)
-
             // Descendo
+            clearInterval(upInterval);
+
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
@@ -41,20 +43,22 @@ function createCactus() {
     let cactusPosition = 1000;
     let randomTime = Math.random() * 6000;
 
+    if (isGameOver) return;
+
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+    cactus.style.left = cactusPosition + 'px';
 
-    let leftInterval = setInterval(() => {
-        cactusPosition -= 10;
-        cactus.style.left = cactusPosition + 'px';
 
+    let leftTimer = setInterval(() => {
         if (cactusPosition < -60) {
-            clearInterval(leftInterval);
+            // saiu da tela
+            clearInterval(leftTimer);
             background.removeChild(cactus);
         } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
             // Game Over
-            clearInterval(leftInterval);
+            clearInterval(leftTimer);
+            isGameOver = true;
             document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         } else {
             cactusPosition -= 10;
